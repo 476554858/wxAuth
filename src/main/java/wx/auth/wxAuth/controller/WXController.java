900128cf731f90e50d971e197781ce6a05ae85f7
476554858@qq.com <476554858@qq.com>
2023-07-01 17:34:17 +0800

    微信授权简单demo

--- src/main/java/wx/auth/wxAuth/controller/WXController.java
+++ src/main/java/wx/auth/wxAuth/controller/WXController.java
@@ -11,23 +11,27 @@ import java.util.Map;
 @RestController
 public class WXController {
 
+    /**
+     * 微信小程序登录
+     *
+     * @param code
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/wxLogin")
-    public Map<String,Object> wxLogin(String code)throws Exception{
-        System.out.println("+++++++++++++++wxlogin-code:"+code);
+    public Map<String, Object> wxLogin(String code) throws Exception {
+        System.out.println("+++++++++++++++wxlogin-code:" + code);
         String APPID = "wx740c40c88326d26b";
         String SECRET = "24f1970e3b267a3bda4beb9115dbd5b1";
         String url = "https://api.weixin.qq.com/sns/jscode2session?appid="
-                +APPID+"&secret="+ SECRET
-                +"&js_code="+code+"&grant_type=authorization_code";
+                + APPID + "&secret=" + SECRET
+                + "&js_code=" + code + "&grant_type=authorization_code";
         JSONObject json = WeixinUtil.doGet(url);
         System.out.println(json);
-        Map<String,Object> map = new HashMap<String, Object>();
-        map.put("code",code);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", code);
         return map;
     }
-
-
-
 
 
 }
